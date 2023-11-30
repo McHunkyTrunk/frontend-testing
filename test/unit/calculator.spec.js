10872a044e8c0f6ef3076af77f85d37d4f76a464
Jaroslav Bobrowski <jbob@campudus.com>
2023-12-01 00:40:02 +0100

    solution task 1.1

--- test/unit/calculator.spec.js
+++ test/unit/calculator.spec.js
@@ -23,35 +23,71 @@ describe("calculator", function () {
     expect(INITIAL_STATE.currentValue).to.equal("0");
   });
 
-  it("...", () => {
-    // TODO test 2
+  it("should replace 0 in initial state", () => {
+    expect(stream("4").currentValue).to.equal("4");
   });
 
-  it("...", () => {
-    // TODO test 3
+  it("should add a digit if not in initial state", () => {
+    expect(stream("34").currentValue).to.equal("34");
   });
 
-  it("...", () => {
-    // TODO test 4
+  it("should not change value if operator appears", () => {
+    expect(stream("3+").currentValue).to.equal("3");
   });
 
-  it("...", () => {
-    // TODO test 5
+  it("should change value to digit when digit appears after operator", () => {
+    expect(stream("37+4").currentValue).to.equal("4");
   });
 
-  it("...", () => {
-    // TODO test 6
+  it("should compute 37+42= to be 79", () => {
+    expect(stream("37+42=").currentValue).to.equal("79");
   });
 
-  it("...", () => {
-    // TODO test 7
+  it("should compute another expression after \"=\"", () => {
+    expect(stream("1+2=4*5=").currentValue).to.equal("20");
   });
 
-  it("...", () => {
-    // TODO test 8
+  it("should enabling using computation result in next computation", () => {
+    expect(stream("1+2=*5=").currentValue).to.equal("15");
+  });
+
+  it("second operator is also an equal", () => {
+    expect(stream("1+2*").currentValue).to.equal("3");
+  });
+
+  it("second operator is also an equal but it can continue after that", () => {
+    expect(stream("1+2*11=").currentValue).to.equal("33");
+  });
+
+  it("+42= should compute to 42", () => {
+    expect(stream("+42=").currentValue).to.equal("42");
+  });
+
+  it("*42= should compute to 0", () => {
+    expect(stream("*42=").currentValue).to.equal("0");
+  });
+
+  it("47-48= should compute to -1", () => {
+    expect(stream("47-48=").currentValue).to.equal("-1");
+  });
+
+  it("8/2= should compute to 4", () => {
+    expect(stream("8/2=").currentValue).to.equal("4");
   });
 
   it("...", () => {
     // TODO test 9
+  });
+
+  it("...", () => {
+    // TODO test 10
+  });
+
+  it("...", () => {
+    // TODO test 11
+  });
+
+  it("...", () => {
+    // TODO test 12
   });
 });
